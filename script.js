91186badbe2c0b08a34844b4bbc7bdf4317acee3
francesjgonzales <francesj7837@gmail.com>
2021-09-14 23:09:31 +0800

    added delete and update

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var selectedRow = null
 function onFormSubmit() {
     var formData = readFormData();
     insertNewRecord(formData);
-    onEdit(td);
+    resertForm();
 }
 
 function readFormData() {
-    var formData = { };
+    var formData = {};
     formData["number"] = document.getElementById("number").value;
     formData["date"] = document.getElementById("date").value;
     formData["time"] = document.getElementById("time").value;
@@ -36,6 +36,13 @@ function insertNewRecord(data) {
 
 }
 
+function resertForm() {
+    document.getElementById("number").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("temperature").value = "";
+    document.getElementById("symptoms").value = "";
+}
+
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById("number").value = selectedRow.cells[0].innerHTML;
@@ -44,4 +51,19 @@ function onEdit(td) {
     document.getElementById("temperature").value = selectedRow.cells[3].innerHTML;
     document.getElementById("symptoms").value = selectedRow.cells[4].innerHTML;
 
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.number;
+    selectedRow.cells[1].innerHTML = formData.date;
+    selectedRow.cells[2].innerHTML = formData.temperature;
+    selectedRow.cells[3].innerHTML = formData.symptoms;
+}
+
+function onDelete(td) {
+    if (confirm('Delete Item?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("healthtracker").deleteRow(row.rowIndex);
+        resertForm();
+    }
 }
